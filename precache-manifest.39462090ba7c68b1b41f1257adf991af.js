81cafc45c6f9e5856e540ec1f1eb536f2ab96b3d
Rajat Gupta <rajatgupta0916@gmail.com>
2020-09-05 23:48:24 +0530

    Updates

---
+++ precache-manifest.39462090ba7c68b1b41f1257adf991af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f576b54a293cb4495ca1b2b9df949db",
+    "url": "/nushk_sportz/index.html"
+  },
+  {
+    "revision": "c54746c4726c92cd85da",
+    "url": "/nushk_sportz/static/css/main.54ba8352.chunk.css"
+  },
+  {
+    "revision": "9086a5f3e3b5734ffe3f",
+    "url": "/nushk_sportz/static/js/2.335cb862.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nushk_sportz/static/js/2.335cb862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54746c4726c92cd85da",
+    "url": "/nushk_sportz/static/js/main.15897c9b.chunk.js"
+  },
+  {
+    "revision": "5189a89ee1b5dc0cfe9c",
+    "url": "/nushk_sportz/static/js/runtime-main.c93e6294.js"
+  },
+  {
+    "revision": "9043bfbbd33077af4788f6cbbd3a5fbc",
+    "url": "/nushk_sportz/static/media/Nushk_logo.9043bfbb.PNG"
+  }
+]);
